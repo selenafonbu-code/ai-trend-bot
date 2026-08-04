5d2c61af3e4c5b14bede3723b0a593e16305d8c0
selenafonbu-code <selenafonbu@gmail.com>
2026-08-04 18:44:19 +0300

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "OK",
+        message: "AI Trend Bot is running 🚀"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
